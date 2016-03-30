3bfcc582a57461444ebb57fd3f413b3342d8df07
Nakul Jeirath <nakuljeirath@Nakuls-MacBook-Pro.local>
2016-03-30 02:13:37 -0500

    Added download all dialog

--- app/dashboard/bucket.ctrl.js
+++ app/dashboard/bucket.ctrl.js
@@ -1,11 +1,32 @@
 'use strict';
 
 angular.module('dashboard')
-.controller('BucketCtrl', ['$scope', 'Lightbox', function($scope, Lightbox) {
+.controller('BucketCtrl', ['$scope', 'Lightbox', '$uibModal', function($scope, Lightbox, $uibModal) {
 	$scope.openLightbox = function(bucketName, index) {
 		var images = $scope.buckets.buckets.find(function(item) {
 			return item.name == bucketName;
 		});
 		Lightbox.openModal(images.files, index);
+	};
+	
+	$scope.downloadModal = function(bucketName) {
+		console.log(bucketName);
+		$uibModal.open({
+			animation: true,
+			templateUrl: 'dashboard/download.modal.html',
+			resolve: {
+				bucket: function() {
+					return bucketName;
+				}
+			},
+			size: 'lg',
+			controller: ['$scope', '$uibModalInstance', 'bucket', function($scope, $uibModalInstance, bucket) {
+				$scope.bucket = bucket;
+				
+				$scope.ok = function () {
+					$uibModalInstance.close();
+				};
+			}]
+		});
 	};
 }]);
