29c997cf685d7191cec9d2e51e2393e2340147f7
Nakul Jeirath <nakuljeirath@Nakuls-MacBook-Pro.local>
2016-04-06 00:22:10 -0500

    Nabvar collapse now working

--- app/app.js
+++ app/app.js
@@ -43,6 +43,9 @@ config(['$stateProvider', function($stateProvider) {
 	  templateUrl: 'tutorial/tutorial.partial.html'
   });
 }])
+.controller('NavCtrl', ['$scope', function($scope) {
+	$scope.navbarCollapsed = true;
+}])
 .run(['$rootScope', '$location', '$window', function($rootScope, $location, $window) {
 	$window.ga('create', 'UA-74793002-1', 'auto');
 	$rootScope.$on('$stateChangeSuccess', function (event) {
